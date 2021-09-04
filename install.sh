ff037d8f3277fac7552ab0981d2752c463792b80
ishuzaks <k.suzuki512+develop@gmail.com>
2021-09-04 09:21:42 +0000

    Add Ansible-playbook execution

--- install.sh
+++ install.sh
@@ -5,3 +5,5 @@ cd $(dirname $0)
 sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get install --yes ansible ansible-lint
 
+cd ${PWD}/playbook
+ansible-playbook --ask-become-pass --connection local --inventory localhost, entry-point.yaml
