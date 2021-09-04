c691f504e2a2004edb9ea4888500f21df6782f34
ishuzaks <k.suzuki512+develop@gmail.com>
2021-09-04 11:49:23 +0000

    Add python3-apt installation

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 cd $(dirname $0)
 
 sudo apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get install --yes ansible ansible-lint
+sudo DEBIAN_FRONTEND=noninteractive apt-get install --yes ansible ansible-lint python3-apt
 
 cd ${PWD}/playbook
 ansible-playbook --ask-become-pass --connection local --inventory localhost, entry-point.yaml
