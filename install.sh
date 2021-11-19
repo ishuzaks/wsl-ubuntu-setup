24835fa0ad32f67fc9efa95dc00f2674521697c5
ishuzaks <k.suzuki512+develop@gmail.com>
2021-11-19 11:31:42 +0900

    Fix ansible installation to the official way.

--- install.sh
+++ install.sh
@@ -5,7 +5,9 @@ cd $(dirname $0)
 DEBIAN_FRONTEND=noninteractive
 
 sudo apt-get update && sudo apt-get --yes upgrade
-sudo apt-get install --yes ansible ansible-lint python3-apt
+sudo apt-get install --yes software-properties-common
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt-get install --yes ansible 
 
 cd ${PWD}/playbook
-ansible-playbook --ask-become-pass --connection local --inventory localhost, main.yaml
+LANG=C ansible-playbook --ask-become-pass --connection local --inventory localhost, main.yaml
