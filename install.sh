8664c6f5486a2a79f732c50b184072993a37f884
ishuzaks <k.suzuki512+develop@gmail.com>
2022-11-10 21:50:29 +0900

    Fixed to specify playbook path as an argument.

--- install.sh
+++ install.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-cd $(dirname $0)
+DEBIAN_FRONTEND=noninteractive sudo apt-get update && sudo apt-get --yes upgrade
+DEBIAN_FRONTEND=noninteractive sudo apt-get install --yes software-properties-common
+DEBIAN_FRONTEND=noninteractive sudo apt-add-repository --yes --update ppa:ansible/ansible
+DEBIAN_FRONTEND=noninteractive sudo apt-get install --yes ansible 
 
-DEBIAN_FRONTEND=noninteractive
-
-sudo apt-get update && sudo apt-get --yes upgrade
-sudo apt-get install --yes software-properties-common
-sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt-get install --yes ansible 
-
-cd ${PWD}/playbook
-LANG=C ansible-playbook --ask-become-pass --connection local --inventory localhost, main.yaml
+LANG=C ansible-playbook --ask-become-pass --connection local --inventory localhost, "$1"
 sudo apt-get autoremove --yes
